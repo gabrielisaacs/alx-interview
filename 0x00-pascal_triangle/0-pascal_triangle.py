1833811c9abc109b4c6a896b5e303dda2038f29e
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-03 14:12:37 +0100

    fix bug

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,13 @@
 def pascal_triangle(n):
-    """
-    Generate Pascal's Triangle of size n.
-    Returns an empty list if n <= 0.
-    """
     if n <= 0:
         return []
     
     triangle = []
     
     for i in range(n):
-        row = [1] * (i + 1)  # Start with a row of 1s
+        row = [1] * (i + 1)  # Start with a row filled with 1s
         for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]  # Sum the two values above
         triangle.append(row)
     
     return triangle
