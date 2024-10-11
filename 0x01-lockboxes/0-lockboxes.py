1cde5e2095c4e6bc7b5e7c4c0fb51457f662eed0
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-11 05:51:17 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Defining a function that determines if
+all the boxes can be opened.
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked starting from box 0.
+
+    Args:
+        boxes (list of lists): A list where each index represents a box
+                               and the list at that index contains the keys
+                               to other boxes.
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    # Create a set to track the opened boxes (starting with the first box)
+    opened_boxes = set([0])
+    
+    # Stack to track boxes that need to be opened
+    keys_stack = [0]
+    
+    # Process each box using DFS
+    while keys_stack:
+        current_box = keys_stack.pop()
+        
+        # Go through each key in the current box
+        for key in boxes[current_box]:
+            if key not in opened_boxes and key < len(boxes):  # If the box isn't opened yet and the key is valid
+                opened_boxes.add(key)
+                keys_stack.append(key)  # Add the new box to the stack to unlock it
+    
+    # If all boxes have been opened, return True
+    return len(opened_boxes) == len(boxes)
+
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,11 @@
+# Lockboxes
+
+This project involves solving the Lockboxes problem in Python.
+
+## Problem Description
+You are given `n` number of locked boxes in front of you. Each box is numbered sequentially from 0 to `n - 1` and each box may contain keys to other boxes. The objective is to determine if all boxes can be opened starting from box 0.
+
+### Prototype
+```python
+def canUnlockAll(boxes):
+
